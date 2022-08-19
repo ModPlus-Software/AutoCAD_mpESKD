9f85d19b76b4d4e387663be6da653d48f3c1d3c5
Yusuf <umr5@mail.ru>
2022-08-19 11:35:06 +0500

    конфликты после ревью

--- mpESKD/Functions/mpView/Grips/ViewTextGrip.cs
+++ mpESKD/Functions/mpView/Grips/ViewTextGrip.cs
@@ -1,82 +1,81 @@
-﻿namespace mpESKD.Functions.mpView.Grips
+﻿namespace mpESKD.Functions.mpView.Grips;
+
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Base.Enums;
+using Base.Overrules;
+using Base.Utils;
+using ModPlusAPI;
+using ModPlusAPI.Windows;
+using View = mpView.View;
+
+/// <summary>
+/// Ручка позиции текста
+/// </summary>
+public class ViewTextGrip : SmartEntityGripData
 {
-    using Autodesk.AutoCAD.DatabaseServices;
-    using Autodesk.AutoCAD.Runtime;
-    using Base.Enums;
-    using Base.Overrules;
-    using Base.Utils;
-    using ModPlusAPI;
-    using ModPlusAPI.Windows;
-    using View = mpView.View;
+    public ViewTextGrip(View view)
+    {
+        View = view;
+        GripType = GripType.Point;
+        CachedAlongTopShelfTextOffset = view.AlongTopShelfTextOffset;
+        CachedAcrossTopShelfTextOffset = view.AcrossTopShelfTextOffset;
+    }
 
     /// <summary>
-    /// Ручка позиции текста
+    /// Экземпляр класса View
     /// </summary>
-    public class ViewTextGrip : SmartEntityGripData
+    public View View { get; }
+
+    /// <summary>
+    /// Имя ручки, чтобы определить от какого она текста
+    /// </summary>
+    public string TextGripName { get; set; }
+
+    public double CachedAlongTopShelfTextOffset { get; }
+
+    public double CachedAcrossTopShelfTextOffset { get; }
+
+    public override string GetTooltip()
     {
-        public ViewTextGrip(View view)
+        return Language.GetItem("gp1"); // stretch
+    }
+
+    public override void OnGripStatusChanged(ObjectId entityId, Status newStatus)
+    {
+        try
         {
-            View = view;
-            GripType = GripType.Point;
-            CachedAlongTopShelfTextOffset = view.AlongTopShelfTextOffset;
-            CachedAcrossTopShelfTextOffset = view.AcrossTopShelfTextOffset;
-        }
-
-        /// <summary>
-        /// Экземпляр класса View
-        /// </summary>
-        public View View { get; }
-
-        /// <summary>
-        /// Имя ручки, чтобы определить от какого она текста
-        /// </summary>
-        public string TextGripName { get; set; }
-
-        public double CachedAlongTopShelfTextOffset { get; }
-
-        public double CachedAcrossTopShelfTextOffset { get; }
-
-        public override string GetTooltip()
-        {
-            return Language.GetItem("gp1"); // stretch
-        }
-
-        public override void OnGripStatusChanged(ObjectId entityId, Status newStatus)
-        {
-            try
+            // При удачном перемещении ручки записываем новые значения в расширенные данные
+            // По этим данным я потом получаю экземпляр класса view
+            if (newStatus == Status.GripEnd)
             {
-                // При удачном перемещении ручки записываем новые значения в расширенные данные
-                // По этим данным я потом получаю экземпляр класса view
-                if (newStatus == Status.GripEnd)
+                using (var tr = AcadUtils.Database.TransactionManager.StartOpenCloseTransaction())
                 {
-                    using (var tr = AcadUtils.Database.TransactionManager.StartOpenCloseTransaction())
+                    var blkRef = tr.GetObject(View.BlockId, OpenMode.ForWrite, true, true);
+                    using (var resBuf = View.GetDataForXData())
                     {
-                        var blkRef = tr.GetObject(View.BlockId, OpenMode.ForWrite, true, true);
-                        using (var resBuf = View.GetDataForXData())
-                        {
-                            blkRef.XData = resBuf;
-                        }
-
-                        tr.Commit();
+                        blkRef.XData = resBuf;
                     }
 
-                    View.Dispose();
+                    tr.Commit();
                 }
 
-                // При отмене перемещения возвращаем временные значения
-                if (newStatus == Status.GripAbort)
-                {
-                    View.AlongTopShelfTextOffset = CachedAlongTopShelfTextOffset;
-                    View.AcrossTopShelfTextOffset = CachedAcrossTopShelfTextOffset;
-                }
-
-                base.OnGripStatusChanged(entityId, newStatus);
+                View.Dispose();
             }
-            catch (Exception exception)
+
+            // При отмене перемещения возвращаем временные значения
+            if (newStatus == Status.GripAbort)
             {
-                if (exception.ErrorStatus != ErrorStatus.NotAllowedForThisProxy)
-                    ExceptionBox.Show(exception);
+                View.AlongTopShelfTextOffset = CachedAlongTopShelfTextOffset;
+                View.AcrossTopShelfTextOffset = CachedAcrossTopShelfTextOffset;
             }
+
+            base.OnGripStatusChanged(entityId, newStatus);
+        }
+        catch (Exception exception)
+        {
+            if (exception.ErrorStatus != ErrorStatus.NotAllowedForThisProxy)
+                ExceptionBox.Show(exception);
         }
     }
 }
--- mpESKD/Functions/mpView/Grips/ViewVertexGrip.cs
+++ mpESKD/Functions/mpView/Grips/ViewVertexGrip.cs
@@ -1,107 +1,113 @@
-﻿namespace mpESKD.Functions.mpView.Grips
+﻿namespace mpESKD.Functions.mpView.Grips;
+
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Base.Enums;
+using Base.Overrules;
+using Base.Utils;
+using ModPlusAPI;
+using ModPlusAPI.Windows;
+using View = mpView.View;
+
+/// <summary>
+/// Ручка вершин
+/// </summary>
+public class ViewVertexGrip : SmartEntityGripData
 {
-    using System.Collections.Generic;
-    using Autodesk.AutoCAD.DatabaseServices;
-    using Autodesk.AutoCAD.Geometry;
-    using Autodesk.AutoCAD.GraphicsInterface;
-    using Autodesk.AutoCAD.Runtime;
-    using Base.Enums;
-    using Base.Overrules;
-    using Base.Utils;
-    using ModPlusAPI;
-    using ModPlusAPI.Windows;
-    using View = mpView.View;
+    private readonly List<Point3d> _points;
 
     /// <summary>
-    /// Ручка вершин
+    /// Initializes a new instance of the <see cref="ViewVertexGrip"/> class.
     /// </summary>
-    public class ViewVertexGrip : SmartEntityGripData
+    /// <param name="view">Экземпляр класса <see cref="mpView.View"/></param>
+    /// <param name="index">Индекс ручки</param>
+    public ViewVertexGrip(View view, int index)
     {
+        View = view;
+        GripIndex = index;
+        GripType = GripType.Point;
+            
+        /* При инициализации ручки нужно собрать все точки вида и поместить их в поле _points.
+         * Это создаст кэш точек вида. Если в методе WorldDraw брать точки из самого вида (View),
+         * то вспомогательные линии будут меняться при зуммировании. Это связано с тем, что в методе
+         * MoveGripPointsAt происходит вызов метода UpdateEntities */
+        _points = new List<Point3d> { View.InsertionPoint };
+        _points.Add(View.EndPoint);
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ViewVertexGrip"/> class.
-        /// </summary>
-        /// <param name="view">Экземпляр класса <see cref="mpView.View"/></param>
-        /// <param name="index">Индекс ручки</param>
-        public ViewVertexGrip(View view, int index)
+    /// <summary>
+    /// Экземпляр класса <see cref="mpView.View"/>
+    /// </summary>
+    public View View { get; }
+
+    /// <summary>
+    /// Индекс ручки
+    /// </summary>
+    public int GripIndex { get; }
+
+    /// <inheritdoc />
+    public override string GetTooltip()
+    {
+        return Language.GetItem("gp1"); // stretch
+    }
+
+    // Временное значение ручки
+    private Point3d _gripTmp;
+
+    /// <inheritdoc />
+    public override void OnGripStatusChanged(ObjectId entityId, Status newStatus)
+    {
+        try
         {
-            View = view;
-            GripIndex = index;
-            GripType = GripType.Point;
-        }
-
-        /// <summary>
-        /// Экземпляр класса <see cref="mpView.View"/>
-        /// </summary>
-        public View View { get; }
-
-        /// <summary>
-        /// Индекс ручки
-        /// </summary>
-        public int GripIndex { get; }
-
-        /// <inheritdoc />
-        public override string GetTooltip()
-        {
-            return Language.GetItem("gp1"); // stretch
-        }
-
-        // Временное значение ручки
-        private Point3d _gripTmp;
-
-        /// <inheritdoc />
-        public override void OnGripStatusChanged(ObjectId entityId, Status newStatus)
-        {
-            try
+            // При начале перемещения запоминаем первоначальное положение ручки
+            // Запоминаем начальные значения
+            if (newStatus == Status.GripStart)
             {
-                // При начале перемещения запоминаем первоначальное положение ручки
-                // Запоминаем начальные значения
-                if (newStatus == Status.GripStart)
-                {
-                    _gripTmp = GripPoint;
-                }
+                _gripTmp = GripPoint;
+            }
 
-                // При удачном перемещении ручки записываем новые значения в расширенные данные
-                // По этим данным я потом получаю экземпляр класса section
-                if (newStatus == Status.GripEnd)
+            // При удачном перемещении ручки записываем новые значения в расширенные данные
+            // По этим данным я потом получаю экземпляр класса section
+            if (newStatus == Status.GripEnd)
+            {
+                using (var tr = AcadUtils.Database.TransactionManager.StartOpenCloseTransaction())
                 {
-                    using (var tr = AcadUtils.Database.TransactionManager.StartOpenCloseTransaction())
+                    var blkRef = tr.GetObject(View.BlockId, OpenMode.ForWrite, true, true);
+                    using (var resBuf = View.GetDataForXData())
                     {
-                        var blkRef = tr.GetObject(View.BlockId, OpenMode.ForWrite, true, true);
-                        using (var resBuf = View.GetDataForXData())
-                        {
-                            blkRef.XData = resBuf;
-                        }
-
-                        tr.Commit();
+                        blkRef.XData = resBuf;
                     }
 
-                    View.Dispose();
+                    tr.Commit();
                 }
 
-                // При отмене перемещения возвращаем временные значения
-                if (newStatus == Status.GripAbort)
+                View.Dispose();
+            }
+
+            // При отмене перемещения возвращаем временные значения
+            if (newStatus == Status.GripAbort)
+            {
+                if (_gripTmp != null)
                 {
-                    if (_gripTmp != null)
+                    if (GripIndex == 0)
                     {
-                        if (GripIndex == 0)
-                        {
-                            View.InsertionPoint = _gripTmp;
-                        }
-                        else if (GripIndex == 1)
-                        {
-                            View.EndPoint = _gripTmp;
-                        }
+                        View.InsertionPoint = _gripTmp;
+                    }
+                    else if (GripIndex == 1)
+                    {
+                        View.EndPoint = _gripTmp;
                     }
                 }
+            }
 
-                base.OnGripStatusChanged(entityId, newStatus);
-            }
-            catch (Exception exception)
-            {
-                if (exception.ErrorStatus != ErrorStatus.NotAllowedForThisProxy)
-                    ExceptionBox.Show(exception);
-            }
+            base.OnGripStatusChanged(entityId, newStatus);
+        }
+        catch (Exception exception)
+        {
+            if (exception.ErrorStatus != ErrorStatus.NotAllowedForThisProxy)
+                ExceptionBox.Show(exception);
         }
     }
 }
