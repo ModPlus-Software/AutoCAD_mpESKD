12573b5e3fbbae2bdbe58497324387f2a06405be
Александр Пекшев <cjmodis@gmail.com>
2022-09-06 14:00:06 +0300

    Improve Grip ContextMenu

---
+++ mpESKD/Base/Overrules/Grips/ContextMenuHost.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace mpESKD.Base.Overrules.Grips;
+
+/// <summary>
+/// Логика взаимодействия для ContextMenuHost.xaml
+/// </summary>
+public partial class ContextMenuHost
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ContextMenuHost"/> class.
+    /// </summary>
+    public ContextMenuHost()
+    {
+        InitializeComponent();
+    }
+}
--- mpESKD/Functions/mpNodalLeader/Grips/NodalFrameTypeGrip.cs
+++ mpESKD/Functions/mpNodalLeader/Grips/NodalFrameTypeGrip.cs
@@ -1,21 +1,20 @@
-﻿
-namespace mpESKD.Functions.mpNodalLeader.Grips;
+﻿namespace mpESKD.Functions.mpNodalLeader.Grips;
 
 using Autodesk.AutoCAD.DatabaseServices;
-using Base.Enums;
 using Base.Overrules;
 using Base.Utils;
 using ModPlusAPI;
-using System;
 using System.Windows;
 using System.Windows.Controls;
-using Autodesk.AutoCAD.DatabaseServices;
-using mpESKD.Base.Utils;
+using Base.Overrules.Grips;
+
 /// <summary>
 /// Ручка выбора типа рамки, меняющая тип рамки
 /// </summary>
 public class NodalFrameTypeGrip : SmartEntityGripData
 {
+    private ContextMenuHost _win;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="NodalFrameTypeGrip"/> class.
     /// </summary>
@@ -43,19 +42,67 @@ public class NodalFrameTypeGrip : SmartEntityGripData
     {
         using (NodalLeader)
         {
+            _win = new ContextMenuHost(NodalLeader);
+            _win.Show();
 
-            NodalLeaderFrameTypeMenu nodalLeaderFrameTypeMenu = new NodalLeaderFrameTypeMenu(NodalLeader);
-            nodalLeaderFrameTypeMenu.Show();
+            ContextMenu cm;
 
+            _win.Loaded += (_, _) =>
+            {
+                cm = (ContextMenu)_win.FindResource("Cm");
 
-            nodalLeaderFrameTypeMenu.Loaded += NodalLeaderFrameTypeMenu_Loaded;
+                var menuItem = new MenuItem
+                {
+                    Name = "Round",
+                    IsCheckable = true,
+                    Header = "Round", // todo <-- localization
+                    IsChecked = NodalLeader.FrameType == FrameType.Round
+                };
+                menuItem.Click += MenuItemOnClick;
+                cm.Items.Add(menuItem);
+
+                menuItem = new MenuItem
+                {
+                    Name = "Rectangular",
+                    IsCheckable = true,
+                    Header = "Rectangular", // todo <-- localization
+                    IsChecked = NodalLeader.FrameType == FrameType.Rectangular
+                };
+                menuItem.Click += MenuItemOnClick;
+                cm.Items.Add(menuItem);
+
+                cm.Closed += (_, _) => Autodesk.AutoCAD.Internal.Utils.SetFocusToDwgView();
+
+                cm.IsOpen = true;
+            };
         }
 
         return ReturnValue.GetNewGripPoints;
     }
 
-    private void NodalLeaderFrameTypeMenu_Loaded(object sender, System.Windows.RoutedEventArgs e)
+    private void MenuItemOnClick(object sender, RoutedEventArgs e)
     {
+        _win?.Close();
+
+        var menuItem = (MenuItem)sender;
         
+        NodalLeader.FrameType = menuItem.Name == "Round" ? FrameType.Round : FrameType.Rectangular;
+        
+        NodalLeader.UpdateEntities();
+        NodalLeader.BlockRecord.UpdateAnonymousBlocks();
+        using (AcadUtils.Document.LockDocument())
+        {
+            using (var tr = AcadUtils.Database.TransactionManager.StartOpenCloseTransaction())
+            {
+                var blkRef = tr.GetObject(NodalLeader.BlockId, OpenMode.ForWrite, true, true);
+
+                using (var resBuf = NodalLeader.GetDataForXData())
+                {
+                    blkRef.XData = resBuf;
+                }
+
+                tr.Commit();
+            }
+        }
     }
 }
