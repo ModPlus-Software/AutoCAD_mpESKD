a626b272cd7b618f351dd6f761d26c993122117e
kir222k <u.k.send@gmail.com>
2023-11-12 18:58:16 +0300

    Grips\ThickArrowVertexGrip.cs ThickArrow.cs ThickArrowDescriptor.cs ThickArrowFunction.cs ThickArrowGripPointOverrule.cs

---
+++ mpESKD/Functions/mpThickArrow/Grips/ThickArrowVertexGrip.cs
@@ -0,0 +1,102 @@
+﻿namespace mpESKD.Functions.mpThickArrow.Grips;
+
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Base.Enums;
+using Base.Overrules;
+using Base.Utils;
+using ModPlusAPI;
+using ModPlusAPI.Windows;
+using ThickArrow = mpThickArrow.ThickArrow;
+
+/// <summary>
+/// Ручка вершин
+/// </summary>
+public class ThickArrowVertexGrip : SmartEntityGripData
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ThickArrowVertexGrip"/> class.
+    /// </summary>
+    /// <param name="thickArrow">Экземпляр класса <see cref="mpThickArrow.ThickArrow"/></param>
+    /// <param name="index">Индекс ручки</param>
+    public ThickArrowVertexGrip(ThickArrow thickArrow, int index)
+    {
+        ThickArrow = thickArrow;
+        GripIndex = index;
+        GripType = GripType.Point;
+    }
+
+    /// <summary>
+    /// Экземпляр класса <see cref="mpThickArrow.ThickArrow"/>
+    /// </summary>
+    public ThickArrow ThickArrow { get;  }
+
+    /// <summary>
+    /// Индекс ручки
+    /// </summary>
+    public int GripIndex { get; }
+
+    /// <inheritdoc />
+    public override string GetTooltip()
+    {
+        return Language.GetItem("gp1"); // stretch
+    }
+
+    // Временное значение ручки
+    private Point3d _gripTmp;
+
+    public override void OnGripStatusChanged(ObjectId entityId, Status newStatus)
+    {
+        try
+        {
+            // При начале перемещения запоминаем первоначальное положение ручки
+            // Запоминаем начальные значения
+            if (newStatus == Status.GripStart)
+            {
+                _gripTmp = GripPoint;
+            }
+
+            // При удачном перемещении ручки записываем новые значения в расширенные данные
+            // По этим данным я потом получаю экземпляр класса section
+            if (newStatus == Status.GripEnd)
+            {
+                using (var tr = AcadUtils.Database.TransactionManager.StartOpenCloseTransaction())
+                {
+                    var blkRef = tr.GetObject(ThickArrow.BlockId, OpenMode.ForWrite, true, true);
+                    using (var resBuf = ThickArrow.GetDataForXData())
+                    {
+                        blkRef.XData = resBuf;
+                    }
+
+                    tr.Commit();
+                }
+
+                ThickArrow.Dispose();
+            }
+
+            // При отмене перемещения возвращаем временные значения
+            if (newStatus == Status.GripAbort)
+            {
+                if (_gripTmp != null)
+                {
+                    if (GripIndex == 0)
+                    {
+                        ThickArrow.InsertionPoint = _gripTmp;
+                    }
+                    else if (GripIndex == 1)
+                    {
+                        ThickArrow.EndPoint = _gripTmp;
+                    }
+                }
+            }
+
+            base.OnGripStatusChanged(entityId, newStatus);
+        }
+        catch (Exception exception)
+        {
+            if (exception.ErrorStatus != ErrorStatus.NotAllowedForThisProxy)
+                ExceptionBox.Show(exception);
+        }
+    }
+}
---
+++ mpESKD/Functions/mpThickArrow/ThickArrowGripPointOverrule.cs
@@ -0,0 +1,141 @@
+﻿namespace mpESKD.Functions.mpThickArrow;
+
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Base;
+using Base.Overrules;
+using Grips;
+using ModPlusAPI.Windows;
+using mpESKD.Functions.mpView.Grips;
+
+/// <inheritdoc />
+public class ThickArrowGripPointOverrule : BaseSmartEntityGripOverrule<mpThickArrow.ThickArrow>
+{
+    /// <inheritdoc />
+    public override void GetGripPoints(
+        Entity entity, GripDataCollection grips, double curViewUnitSize, int gripSize, Vector3d curViewDir, GetGripPointsFlags bitFlags)
+    {
+        try
+        {
+            if (IsApplicable(entity))
+            {
+                // Удаляю все ручки - это удалит ручку вставки блока
+                grips.Clear();
+
+                var thickArrow = EntityReaderService.Instance.GetFromEntity<mpThickArrow.ThickArrow>(entity);
+                if (thickArrow != null)
+                {
+                    // insertion (start) grip
+                    var vertexGrip = new ThickArrowVertexGrip(thickArrow, 0)
+                    {
+                        GripPoint = thickArrow.InsertionPoint
+                    };
+                    grips.Add(vertexGrip);
+
+                    #region Text grips - не использ.
+
+                    //if (view.TextDesignationPoint != Point3d.Origin && view.HasTextValue())
+                    //{
+                    //    var textGrip = new ViewTextGrip(view)
+                    //    {
+                    //        GripPoint = view.TextDesignationPoint,
+                    //        TextGripName = "TopText"
+                    //    };
+                    //    grips.Add(textGrip);
+                    //}
+
+                    //vertexGrip = new ViewVertexGrip(view, 1)
+                    //{
+                    //    GripPoint = view.TopShelfEndPoint
+                    //};
+                    //grips.Add(vertexGrip);
+                    
+                    #endregion
+                }
+            }
+        }
+        catch (Exception exception)
+        {
+            if (exception.ErrorStatus != ErrorStatus.NotAllowedForThisProxy)
+                ExceptionBox.Show(exception);
+        }
+    }
+
+    /// <inheritdoc />
+    public override void MoveGripPointsAt(
+        Entity entity, GripDataCollection grips, Vector3d offset, MoveGripPointsFlags bitFlags)
+    {
+        try
+        {
+            if (IsApplicable(entity))
+            {
+                foreach (var gripData in grips)
+                {
+                    if (gripData is ThickArrowVertexGrip vertexGrip)
+                    {
+                        var thickArrow = vertexGrip.ThickArrow;
+
+                        if (vertexGrip.GripIndex == 0)
+                        {
+                            ((BlockReference)entity).Position = vertexGrip.GripPoint + offset;
+                        }
+                        else if (vertexGrip.GripIndex == 1)
+                        {
+                            thickArrow.EndPoint = vertexGrip.GripPoint + offset;
+                        }
+
+                        // Вот тут происходит перерисовка примитивов внутри блока
+                        thickArrow.UpdateEntities();
+                        thickArrow.BlockRecord.UpdateAnonymousBlocks();
+                    }
+                    /*else if (gripData is ViewTextGrip textGrip)
+                    {
+                        var view = textGrip.View;
+                        var topShelfVector = (view.InsertionPoint - view.EndPoint).GetNormal();
+                        var topStrokeVector = topShelfVector.GetPerpendicularVector().Negate();
+
+                        var deltaY = topStrokeVector.DotProduct(offset) / view.BlockTransform.GetScale();
+                        var deltaX = topShelfVector.DotProduct(offset) / view.BlockTransform.GetScale();
+
+                        if (double.IsNaN(textGrip.CachedAlongTopShelfTextOffset))
+                        {
+                            view.AlongTopShelfTextOffset = deltaX;
+                        }
+                        else
+                        {
+                            view.AlongTopShelfTextOffset = textGrip.CachedAlongTopShelfTextOffset + deltaX;
+                        }
+
+                        if (double.IsNaN(textGrip.CachedAcrossTopShelfTextOffset))
+                        {
+                            view.AcrossTopShelfTextOffset = deltaY;
+                        }
+                        else
+                        {
+                            view.AcrossTopShelfTextOffset = textGrip.CachedAcrossTopShelfTextOffset + deltaY;
+                        }
+
+                        view.UpdateEntities();
+                        view.BlockRecord.UpdateAnonymousBlocks();
+                    }*/
+                    else
+                    {
+                        base.MoveGripPointsAt(entity, grips, offset, bitFlags);
+                    }
+                }
+            }
+            else
+            {
+                base.MoveGripPointsAt(entity, grips, offset, bitFlags);
+            }
+        }
+        catch (Exception exception)
+        {
+            if (exception.ErrorStatus != ErrorStatus.NotAllowedForThisProxy)
+                ExceptionBox.Show(exception);
+        }
+    }
+
+
+}
