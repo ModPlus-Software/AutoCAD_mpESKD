96da54d5ed6683667850fad2e6c0758048c4e3e0
HIMURA\kir <u.k.send@gmail.com>
2024-02-20 16:39:56 +0300

    Исправлен тестовый метод, статические поля убраны

--- mpESKD/Base/Utils/TextStyleArx.cs
+++ mpESKD/Base/Utils/TextStyleArx.cs
@@ -1,6 +1,9 @@
 ﻿namespace mpESKD.Base.Utils;
 
-using ModPlusAPI.IO;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System.Runtime.InteropServices;
 
 // https://adndevblog.typepad.com/autocad/2012/05/actual-width-and-height-of-a-text-string.html
 // To get the mangled name use dumpbin.exe. For ex :
@@ -8,22 +11,11 @@ using ModPlusAPI.IO;
 // Использовано: dumpbin.exe /exports "...\2013\lib-x64\acdb19.lib" > "...\acdb19.txt"
 // Open the generated acdb19.txt to find the signature
 
-using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.EditorInput;
-using Autodesk.AutoCAD.Runtime;
-using System.Runtime.InteropServices;
-using Autodesk.AutoCAD.ApplicationServices;
-
 /// <summary>
 /// Методы для работы со стилями текста с использованием импортированных библиотек ObjectARX
 /// </summary>
-internal static class TextStyleArx
+public static class TextStyleArx
 {
-    // ReSharper disable once AccessToStaticMemberViaDerivedType
-    private static readonly Document Doc = Application.DocumentManager.MdiActiveDocument;
-    private static readonly Database Db = Doc.Database;
-    private static readonly Editor Ed = Doc.Editor;
-
     [System.Security.SuppressUnmanagedCodeSecurity]
     [DllImport(
 #if A2013 
@@ -45,7 +37,7 @@ internal static class TextStyleArx
 #elif A2021
         "acdb24.dll",
 #elif A2022
-        @"acdb24.dll",
+        "acdb24.dll",
 #elif A2023
         "acdb24.dll",
 #elif A2024
@@ -54,7 +46,7 @@ internal static class TextStyleArx
         CharSet = CharSet.Unicode,
         CallingConvention = CallingConvention.Cdecl,
         EntryPoint = "?fromAcDbTextStyle@@YA?AW4ErrorStatus@Acad@@AEAVAcGiTextStyle@@AEBVAcDbObjectId@@@Z")]
-    private static extern ErrorStatus fromAcDbTextStyle(System.IntPtr style, ref ObjectId id); 
+    private static extern ErrorStatus fromAcDbTextStyle(System.IntPtr style, ref ObjectId id);
 
     /// <summary>
     /// Для тестирования размеров текста, с учетом стиля текста
@@ -62,18 +54,40 @@ internal static class TextStyleArx
     [CommandMethod("mpTextTrueWidthMessage")]
     public static void TrueTextSizeMessage()
     {
-        PromptResult prText = Ed.GetString("\nEnter a String: ");
-        PromptResult prTextStyle = Ed.GetString("\nEnter a Text Style: ");
-        PromptResult prTextHeight = Ed.GetDouble("\nEnter a Text Height: ");
+#if DEBUG
+        var ed = AcadUtils.Editor;
 
-        if (prText.Status != PromptStatus.OK 
-            && prTextStyle.Status != PromptStatus.OK 
-            && prTextHeight.Status != PromptStatus.OK)
+        PromptResult prText = ed.GetString("\nEnter a String: ");
+        PromptResult prTextStyle = ed.GetString("\nEnter a Text Style: ");
+
+        if (!TextStyleUtils.HasTextStyle(prTextStyle.StringResult))
+        {
+            AcadUtils.WriteMessageInDebug("There is no such style!");
             return;
+        }
 
-        var textSize = GetTrueTextSize(prText.StringResult, prTextStyle.StringResult, prTextHeight.StringResult.ToDouble());
+        PromptDoubleResult prTextHeight = ed.GetDouble("\nEnter a Text Height: ");
 
-        Ed.WriteMessage($"\nWidth - {textSize.Item1}\nHeight - {textSize.Item2}");
+        try
+        {
+            if (prText.Status != PromptStatus.OK
+                && prTextStyle.Status != PromptStatus.OK
+                && prTextHeight.Status != PromptStatus.OK)
+                return;
+
+            var textSize = GetTrueTextSize(prText.StringResult, prTextStyle.StringResult, prTextHeight.Value);
+
+            AcadUtils.WriteMessageInDebug($"Text: {prText.StringResult}, " +
+                                          $"Height: {prTextHeight.Value}, " +
+                                          $"Style: {prTextStyle.StringResult} => " +
+                                          $"Width: {textSize.Item1}, " +
+                                          $"Height: {textSize.Item2}");
+        }
+        catch (Exception e)
+        {
+            AcadUtils.WriteMessageInDebug($"e.Source: {e.Source}; e.StackTrace: {e.StackTrace}");
+        }
+#endif
     }
 
     /// <summary>
@@ -82,27 +96,26 @@ internal static class TextStyleArx
     /// <param name="text">Текст</param>
     /// <param name="textStyleName">Стиль текста</param>
     /// <param name="textHeight">Высота текста</param>
-    /// <returns>Кортеж (<see cref="double"/> , <see cref="double"/>):  (ширина_текста , высота_текста)</returns>
+    /// <returns>Кортеж (<see cref="double"/>, <see cref="double"/>):  (ширина_текста, высота_текста)</returns>
     /// <remarks>Применяется для многострочного текста <see cref="MText"/></remarks>
     internal static (double, double) GetTrueTextSize(string text, string textStyleName, double textHeight)
     {
         double width = 0.0;
         double height = 0.0;
+        var db = AcadUtils.Database;
 
-        using (Transaction tr = Db.TransactionManager.StartTransaction())
+        using (Transaction tr = db.TransactionManager.StartTransaction())
         {
-            TextStyleTable textStyleTable = tr.GetObject(Db.TextStyleTableId, OpenMode.ForRead) as TextStyleTable;
+            TextStyleTable textStyleTable = tr.GetObject(db.TextStyleTableId, OpenMode.ForRead) as TextStyleTable;
 
             if (textStyleTable != null && textStyleTable.Has(textStyleName))
             {
                 var textStyleId = textStyleTable[textStyleName];
-
                 Autodesk.AutoCAD.GraphicsInterface.TextStyle iStyle = new Autodesk.AutoCAD.GraphicsInterface.TextStyle();
 
                 if (fromAcDbTextStyle(iStyle.UnmanagedObject, ref textStyleId) == ErrorStatus.OK)
                 {
                     iStyle.TextSize = textHeight;
-
                     Extents2d extents = iStyle.ExtentsBox(text, false, true, null);
 
                     width = extents.MaxPoint.X - extents.MinPoint.X;
